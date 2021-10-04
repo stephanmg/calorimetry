571623c0e34fdc94597489dfb92a697c277850c5
Stephan Grein <stephan.grein@uni-bonn.de>
2021-10-04 18:25:52 +0200

    Adding shinyWidgets dependency.

--- server.R
+++ server.R
@@ -3,26 +3,19 @@ library(ggplot2)
 
 # Create server -------------------------------------------------------
 server <- function(input, output, session) {
-   observeEvent(
-      input$plotting,
-      {
-       output$plot <- renderPlot({
 
-   if (is.null(input$File)) {
-   } else {
-   file = input$File
-   C1 <- read.csv2(file$name, header = F, skip = 10, na.strings = c("-","NA"))
-  
-   p <- ggplot(data=C1, aes_string(x="V4", y=input$feature)) + 
-   geom_boxplot() 
-
-   p
-   }
-   
-  })
-
-      }
-   )
-
+   observeEvent(input$plotting, {
+      output$plot <- renderPlot({
+         if (is.null(input$File)) {
+            print("No data given!");
+         } else {
+           file = input$File
+           C1 <- read.csv2(file$name, header = F, skip = 10, na.strings = c("-","NA"))
+           p <- ggplot(data=C1, aes_string(x="V4", y=input$feature)) + 
+           geom_boxplot() 
+           p
+        }
+      })
+    })
 }
 
