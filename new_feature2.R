43ed2b29277e37f3f64b790b36d33eb6a0ee2598
stephanmg <stephan@syntaktischer-zucker.de>
2022-08-30 16:03:51 +0200

    Adding new feature2.R.

---
+++ new_feature2.R
@@ -0,0 +1,25 @@
+extract_rmr_helper <- function() {
+   df <- read.csv2("df_for_cov_analysis.csv")
+   library(dplyr)
+   source("new_feature.R")
+   res <- df %>%
+      group_by(Animal) %>%
+      group_map(~ extract_rmr2(.x, 1, 1))
+   animal_names <- as.data.frame(df %>% group_by(Animal) %>% select(Animal) %>% 
+      distinct(Animal))
+
+   total_data <- data.frame()
+   for (animal in 1:length(res)) {
+      data <- res[[animal]]$df_plot_total
+      tmp <- cbind(c(data$HP), c(data$Time), c(data$Component), c(rep(animal_names$Animal[animal], length(data$Component)))) # nolint
+      total_data <- rbind(total_data, tmp)
+   }
+
+
+   colnames(total_data) <- c("HP", "Time", "Component", "Animal")
+   total_data$Animal <- as.factor(total_data$Animal)
+   total_data$Component <- as.factor(total_data$Component)
+   print(total_data)
+   return(total_data)
+   # FIXME: total data contains O2 and CO2 production not heat production
+}
