8cd0b133e30cf2742da9f9cabb8ff5e643a4da6c
stephanmg <stephan@syntaktischer-zucker.de>
2022-12-30 10:33:13 +0100

    Updating GHA workflow and code commenting.

--- new_feature.R
+++ new_feature.R
@@ -4,65 +4,69 @@ library(dplyr)
 library(ggpubr)
 library(patchwork)
 
-#########################
-# real data
-#########################
-# N = 800 # total intervals
-# M = 50 # number of data points in an interval (needed for
-# time trace reconstruction over 24h day)
-# SLIDING_WINDOW_OF_PREPROCESSING = 10
-# data <- read.csv2("df_for_cov_analysis.csv", sep=";")
-
-do_extract <- function(df, component="O2", percentage=5, N) {
+################################################################################
+# do_extract
+################################################################################
+# N specifies the number of total intervals of time increase of e.g. 15 minutes
+# M specifies the number of intervals one wishes to find a minimum energy exp.
+# percentage specifies how many of the minimum values should be considered
+do_extract <- function(df, component = "O2", percentage = 5, N) {
    # order df by component O2
-   df_ordered = df[order(df[[component]]), ]
+   df_ordered <- df[order(df[[component]]), ]
 
-   # print 
-   indices = which.minn(df_ordered[[component]], n=N*percentage/100)
-
-   sub_df = df_ordered[indices, ]
+   # indices of minimum energy expenditure
+   indices <- which.minn(df_ordered[[component]], n = N * percentage / 100)
+   # extract a sub data frame from the indices
+   sub_df <- df_ordered[indices, ]
    print(sub_df)
 
-   psum <- function(...,na.rm=FALSE) { 
-       rowSums(do.call(cbind,list(...)),na.rm=na.rm) 
-        }
+   # partial rowsum
+   psum <- function(..., na.rm = FALSE) {
+       rowSums(do.call(cbind, list(...)), na.rm = na.rm)
+   }
 
+   # apply partial rowsum to CoV1 (O2) and CoV2 (CO2)
    dat <- transform(sub_df, sum = psum(CoV1, CoV2))
    print(dat)
 
-   # today do not return only the best but the 10% lowest in the interval
-   index = which.minn(dat$sum, n=1) # TODO: return n=3 best values, then do binned scatter plot (y axis)
-   # plot data of heat production by using index
+   # TODO: return n > 1 best values, then create a binned scatter plot (y axis)
+   index <- which.minn(dat$sum, n = 1)
+   # plot data of minimum energy expenditure per interval with one index
    print("RMR in this measurement (interval):")
-   # TODO: HP needs to be calculated with O2 and CO2 and all indices... (index from above!)
+
+   # TODO: HP needs to be calculated with O2 and CO2 an all indices n > 1
+   # HP means heat production, either sorted by O2 or CO2
    print(dat[index, ]$HP)
    dat[index, ]$HP
-   # HP means really heat production, either sorted by O2 or CO2 production or not
 }
 
+################################################################################
+# create_df
+################################################################################
+# df, data frame
+# component, either O2 or CO2
+# M, sliding window size, typically much smaller than N
+# N, total intervals
+# percentage, how many of best (minimum energy expenditure values) to consider
 create_df <- function(df, component, M, N, percentage) {
-   hp = c()
-   index = c()
-
-   # TODO: Time N/M -> N number of data entries, equally spaced typically 10 minutes, M how many intervals...
-   for (i in 0:floor(N/M)) { # sub interval, get minimum of every M elements...
-      # extract only from some interval (could also extract from whole df!)
+   hp <- c()
+   index <- c()
+   for (i in 0:floor(N/M)) { # sub interval, get minimum EE in M intervals
       hp_val <- do_extract(df[seq(i*M, i*M+M),], component, percentage, N)
-      if (length(hp_val) != 0) { # means couldn't extract a minimum in the function do_extract: question figure out why... TODO
+      # Minimum EE couldn't be extracted, but why? (TODO: End of intervals?)
+      if (length(hp_val) != 0) {
          hp <- append(hp, hp_val)
          index <- append(index, i)
       }
    }
    df_plot <- data.frame(hp, index)
    colnames(df_plot) <- c("HP", "Time")
-   # TODO: Detect automatically from the first two measurements (Time2-Time1)
-   INTERVAL_LENGTH = 15
-   #df_plot$Time <- df_plot$Time * floor(N/M) # which averaging (time points in dat taken every 5 minutes?)
+   # TODO: Detect length from metadata or 1st two measurements (Time2-Time1)
+   INTERVAL_LENGTH <- 15
    df_plot$Time <- df_plot$Time * INTERVAL_LENGTH  * (M / INTERVAL_LENGTH)
    df_plot$HP <- df_plot$HP / 24 / (60/INTERVAL_LENGTH) / INTERVAL_LENGTH # check for correctness here TODO
    #df_plot$HP <- df_plot$HP / 24 / 6 # heat production divided by 24 hours (might be scaled wrongly here) 
-   # TODO: need to scale HP to hour first, as it may be only one single value extracted, is has to be accounted for the time interval length
-   # TODO: Interval llength needs to be known 5 or 10 minutes to scale HP...
+   #df_plot$Time <- df_plot$Time * floor(N/M) # which averaging (time points in dat taken every 5 minutes?)
    df_plot
 }
 
