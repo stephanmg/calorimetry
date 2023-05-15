118081f2389167b1e0a49d3899c282ebc9d9b512
stephanmg <stephan@syntaktischer-zucker.de>
2023-05-15 15:04:48 +0200

    More reorganizing.

---
+++ test_for_rmr.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+
+df <- read.csv2("test_for_rmr.csv")
+df$Animal <- as.factor(df$Animal)
+df$Component <- as.factor(df$Component)
+
+p <- ggplot(df, aes(x=Animal, y=HP, color=Component)) + geom_boxplot() + geom_point(position=position_jitter(0.1))
+ggsave("test_for_rmr.png")
+print(df)
