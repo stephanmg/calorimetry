6ff69d2d67680556879a7c798d197d25b79f1900
stephanmg <stephan@syntaktischer-zucker.de>
2023-08-01 12:54:49 +0200

    Cleanup startapp.R

--- startapp.R
+++ startapp.R
@@ -1,7 +1,21 @@
 library(shiny)
 library(shinyFiles)
-options(shiny.host = "0.0.0.0")
-options(shiny.port = 1338)
-options(shiny.maxRequestSize = 30 * 1024^2)
-print(sessionInfo())
+
+# configuration
+port <- 1338
+host <- "0.0.0.0"
+maxRequestSize <- 30 * 1024^2 # 30 MiB
+printSessionInfo <- TRUE
+
+# set Shiny options
+options(shiny.host = host)
+options(shiny.port = port)
+options(shiny.maxRequestSize = maxRequestSize)
+
+# debug
+if (printSessinInfo) {
+   print(sessionInfo())
+}
+
+# start app
 runApp(".")
