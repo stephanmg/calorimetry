c1f8e1131d8f7f6e31648ddd632b1b28ed9c1510
stephanmg <stephan@syntaktischer-zucker.de>
2024-03-01 17:35:22 +0100

    Fix CI?

--- tests/testthat/test_backend.R
+++ tests/testthat/test_backend.R
@@ -9,8 +9,7 @@ test_that("get_true_metadata", {
    expect_equal(ncol(df), 6)
    expect_equal(nrow(df), 16)
    expect_equal(colnames(df), c("lean_mass", "fat_mass", "Animals", "Diet", "Genotype", "body_weight"))
-}
-)
+})
 
 test_that("get_constants", {
    df <- get_constants("../../MetaDataSheet_UCP1\ KO_cb8eb8c0-aa63-4a25-a495-6eaa8dc4a243 (2).xlsx")
