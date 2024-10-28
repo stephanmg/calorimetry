fbe0e3afca49fad6d6cd13f312b6435beb0e1e68
Stephan Grein <stephan.grein@uni-bonn.de>
2024-10-28 16:50:53 +0100

    Removed debug output.

--- generate_man.R
+++ generate_man.R
@@ -3,14 +3,12 @@ library(roxygen2)
 
 load_code_from_inc <- function(path) {
    env <- new.env(parent = globalenv())
-   r_files <- list.files(file.path(path, "inc"), pattern = "\\.R$", full.names = TRUE)
+   r_files <- list.files(file.path(path, "inc"), pattern = "\\.R$", full.names = TRUE, recursive = TRUE)
    for (file in r_files) {
       sys.source(file, envir = env)
    }
    env
 }
 
-
 env = load_code_from_inc("/home/stephan/Documents/Projects/BATEnergy")
-ls(env)
 roxygen2::roxygenise(load_code = load_code_from_inc, roclets = c("rd", "namespace"))
