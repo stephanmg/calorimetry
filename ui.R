48ea9d557566cb23f3eab8283389edded5480922
Stephan Grein <stephan.grein@uni-bonn.de>
2024-11-19 18:34:23 +0100

    Changed to cubic regression splines in smoothing.

--- ui.R
+++ ui.R
@@ -410,7 +410,7 @@ main_content <- mainPanel(
             conditionalPanel("input.add_average_with_se == true", selectInput("averaging_method_with_facets", "Method for smoothing", choices=c("gam", "loess", "lm"), selected="gam")),
             conditionalPanel("input.add_average_with_se == true", numericInput("averaging_method_with_facets_confidence_levels", "SE", min=1, max=10, value=2)),
             conditionalPanel("input.add_average_with_se == true", numericInput("averaging_method_with_facets_basis_functions", "Number of Basis functions", min = 10, max=40, value=20)),
-            conditionalPanel("input.add_average_with_se == true", selectInput("averaging_method_with_facets_basis_function", "Basis function", choices=c("cs", "tp", "cr", "ps", "gp", "ts"), selected="cs")),
+            conditionalPanel("input.add_average_with_se == true", selectInput("averaging_method_with_facets_basis_function", "Basis function", choices=c("cs", "tp", "cr", "ps", "gp", "ts"), selected="cr")),
             conditionalPanel("input.add_average_with_se == true", numericInput("averaging_method_with_facets_alpha_level", "Transparency level", min=0.0, max=1.0, value=0.2, step=0.05)),
             conditionalPanel("input.add_average_with_se == true", colourInput("averaging_method_with_facets_color", "Color", "blue"))
 
