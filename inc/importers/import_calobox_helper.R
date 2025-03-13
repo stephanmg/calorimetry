c85d8a3c34c8db53fe7e7c3bbe03d07fa7a89197
Stephan Grein <stephan.grein@uni-bonn.de>
2025-03-13 15:06:22 +0100

    Finalziing wavelet analysis and fixes for calobox import.

--- inc/importers/import_calobox_helper.R
+++ inc/importers/import_calobox_helper.R
@@ -32,30 +32,40 @@ import_calobox <- function(filename, file_out) {
    df <- df %>% mutate(vH2O..mL.min. = vH2O..mL.min. * 60)
    df <- df %>% mutate(HP.mW. = HP.mW. * 0.0036)
 
+   df$group_index <- cumsum(c(TRUE, diff(df$Function != "Zero") > 0))
+   last_x_indices <- df %>%
+      mutate(Index = row_number()) %>%
+      filter(Function == "Zero") %>%
+      group_by(group_index) %>%
+      summarize(LastX = max(Index)) %>%
+      pull(LastX)
+
+   indices_to_remove <- NULL
+   difference_df = df %>%  filter(Function == "Measure") %>% slice_head(n=2)
+   difference_df <- df %>% mutate(Time. = as.POSIXct(Time., format="%H:%M:%S", tz = "UTC"))
+
+   difference <- as.numeric(difftime(difference_df$Time.[2], difference_df$Time.[1], units = "secs"))
+
+   if (difference == 4) {
+      indices_to_remove <- sort(c(last_x_indices + 1, last_x_indices + 2, last_x_indices + 3, last_x_indices + 4, last_x_indices + 5, last_x_indices + 6, last_x_indices + 7))
+   } else if (difference == 10) {
+      indices_to_remove <- sort(c(last_x_indices+1, last_x_indices + 2, last_x_indices + 3))
+   } else if (difference == 15) {
+      indices_to_remove <- sort(c(last_x_indices+1, last_x_indices + 2))
+   } else if (difference == 30) {
+      indices_to_remove <- sort(c(last_x_indices+1))
+   } else if (difference >= 60) {
+      # No Measurements after Zero Adjustments should be removed
+   }
+
+   indices_to_remove <- indices_to_remove[indices_to_remove <= nrow(df)]
+
+   df <- df %>% filter(!(row_number() %in% indices_to_remove))
 
    # measurements
    df <- df %>% filter(Function == "Measure")
    df <- df %>% select(c(Date.Time, Time., X.RER., HP.mW., VO2.mL.min., CO2.prod..mL.min., vH2O..mL.min., AirPressure..kPa., EE.cal.min.))
-   # TODO: CaloBox measures below 1 minute, which we do not support currently, thus we average two values each
-   # This should be in principle not necessary anymore, see timeScale feature in backend
-   #df <- df %>% mutate(group=rep(1:(nrow(df) %/% 2 + (nrow(df) %% 2 > 0)), each = 2, length.out = nrow(df))) %>%
-   #             group_by(group) %>%
-   #             summarize(
-   #             Date.Time = first(Date.Time),
-   #             Time. = first(Time.),
-   #             X.RER. = mean(X.RER.),
-   #             HP.mW. =  mean(HP.mW.),
-   #             VO2.mL.min. = mean(VO2.mL.min.),
-   #             CO2.prod..mL.min. = mean(CO2.prod..mL.min.),
-   #             AirPressure..kPa. = mean(AirPressure..kPa.),
-   #             EE.cal.min. = mean(EE.cal.min.),
-   #             vH2O..mL.min. = mean(vH2O..mL.min.)) %>% select(-group)
-
-
    df <- df %>% mutate(across(where(is.numeric), ~ gsub("\\.", ",", as.character(.)))) # replace "." with "," for TSE 6.3 format
-
-
-   print(head(df))
 
    df$AnimalID <- 1
    df$Box <- 1
@@ -68,8 +78,6 @@ import_calobox <- function(filename, file_out) {
    df <- df %>% rename(`VH2O(3)`=vH2O..mL.min.)
    df <- df %>% rename(`EE`=EE.cal.min.)
    df <- df %>% rename(`AirPressure`=AirPressure..kPa.)
-
-   print(head(df))
 
    # units and count number of fields 
    units <- c("", "", "", "[kJ/h]", "[ml/h]", "[ml/h]", "[kPa]", "[kcal/min]", "[ml/h]", "", "", "[kJ/h]")
---
+++ inc/statistics/wavelet_analysis.R
@@ -0,0 +1,67 @@
+library(WaveletComp)
+library(ggplot2)
+
+################################################################################
+#' do_add_wavelet_analysis_alt
+#' 
+#' This function performs wavelet analysis of a time signal in hours
+#' @param df
+do_add_wavelet_analysis_alt <- function(df, measurement) {
+	# Get Time consistenly for wavelet function
+	df$Time <- df$running_total.hrs.halfhour
+
+	# Perform wavelet analysis
+	dt_value = mean(diff(df$Time))
+
+	# regular time for the signal (should already be the case, but better to be sure)
+	time_regular <- seq(min(df$Time), max(df$Time), length.out = nrow(df))
+	df <- data.frame(
+		Time = time_regular,
+		Measurement = approx(x = df$Time, y = df[[measurement]], xout = time_regular)$y
+	)
+
+	# now, we also interpolate the measurement onto a regular time grid, 0.5 h interval...
+	time_new <- seq(min(df$Time), max(df$Time), by = 0.5) # since we use the time-scale running_total.hrs.halfhour
+	# otherwise we should interpolate on the regular time grid of diff(df$Time) in seconds (CaloBox timescale)
+	df <- data.frame(
+		Time = time_new,
+		Measurement = approx(x=df$Time, y= df$Measurement, xout=time_new)$y
+	)
+
+	# biwavelet package needs data frame as matrix
+	df = as.matrix(df)
+	wavelet_result <- biwavelet::wt(df)
+
+	wavelet_df <- data.frame(
+		Time = rep(wavelet_result$t, each = length(wavelet_result$period)),
+		Period = rep(wavelet_result$period, times = length(wavelet_result$t)),
+		Power = as.vector(wavelet_result$power)
+	)
+
+	sig_matrix <- wavelet_result$signif
+
+	sig_threshold <- 0.005
+	sig_rows <- apply(sig_matrix, 1, function(row) any(row < sig_threshold, na.rm=TRUE))
+
+	wavelet_power_spectrum <- ggplot(wavelet_df, aes(x = Time, y = Period, fill = Power)) +
+	geom_tile() +
+	scale_fill_viridis_c(option = "plasma") +  
+	labs(title = "Wavelet Power Spectrum", x = "Time", y = "Period (Scale)", fill = "Power") +
+	theme_minimal()
+
+	global_spectrum_df <- data.frame(
+		Period = wavelet_result$period,
+		Power = rowMeans(wavelet_result$power, na.rm = TRUE)
+	)
+
+	sig_global <- global_spectrum_df[sig_rows, ]
+
+	wavelet_global_spectrum <- ggplot(global_spectrum_df, aes(x=Period, y=Power)) +
+	geom_line(color = "blue", size = 1) +
+    geom_point(data = sig_global, aes(x = Period, y = Power), color = "red", size = 2) +
+	labs(title = "Global Wavelet Spectrum", x = "Period (Scale)", y = "Power") +
+	theme_minimal()
+
+	return(list("wavelet_power_spectrum"=wavelet_global_spectrum, "wavelet_plot"=wavelet_power_spectrum))
+	
+}
