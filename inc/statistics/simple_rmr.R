c0d03fd20fc2aa2f34e4fb27d0bdcccaa63d9264
Stephan Grein <stephan.grein@uni-bonn.de>
2025-03-13 17:06:27 +0100

    3 hrs default for simple RMR.

--- inc/statistics/simple_rmr.R
+++ inc/statistics/simple_rmr.R
@@ -1,11 +1,12 @@
 library(ggplot2)
 add_simple_rmr <- function(df, measurement) {
-	window_size <- 3600
+	window_size <- 3600 * 3 # 3 hours window sizes
 
 	dt = mean(diff(df$running_total.sec))
 	print("dt:")
 	print(dt)
 
 	df$rolling_mean <- rollapply(df[[measurement]], width=length(which(df$running_total.sec - df$running_total.sec[1] <= window_size)), FUN=function(x) mean(sort(x)[1:5]), fill = NA, partial=TRUE)
+	#df$rolling_mean <- rollapply(df$HP2, width=length(which(df$running_total.sec - df$running_total.sec[1] <= window_size)), FUN=function(x) mean(sort(x)[1:5]) * 30, fill = NA, partial=TRUE)
 	return(df)
 }
