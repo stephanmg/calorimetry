aa664262f13afa2f8c8df9e70efe32091bee42b1
Stephan Grein <stephan.grein@uni-bonn.de>
2025-03-18 12:07:58 +0100

    Note about starting app with data folders as environment variables.

--- start_app_instances.sh
+++ start_app_instances.sh
@@ -4,6 +4,11 @@
 port=9000
 host=0.0.0.0
 
+# note that app should be started with SHINY_DATA_FOLDER and SHINY_IMPC_FOLDER 
+# environment variables set, as they provide example data and impc database data
+# SHINY_DATA_FOLDER=example_data/
+# SHINY_IMPC_DATA_FOLDER=IMPC_FOLDER/
+
 # if no value provided, assume to be conservative and use only two instances
 HOW_MANY_INSTANCES=$1
 if [ -z "$1" ]; then
