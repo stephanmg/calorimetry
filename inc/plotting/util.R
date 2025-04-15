6f31a6e87d1348f28d26e005a338b75ce858e63e
Stephan Grein <stephan.grein@uni-bonn.de>
2025-04-15 09:44:04 +0200

    Cleanup plotting module.

--- inc/plotting/util.R
+++ inc/plotting/util.R
@@ -9,20 +9,20 @@
 ################################################################################
 add_visualization_type <- function(p, type, with_point=TRUE) {
 	if (type == "Violinplot") { 
-			if (with_point) {
-				p <- p + geom_point()
-			}
-			p <- p + geom_violin(fill="grey80", colour="#3366FF", alpha=0.3) 
-		} else if (type == "Dotplot") {
-			if (with_point) {
-				p <- p + geom_point()
-			}
-			p <- p + geom_boxplot(fill="grey80", colour="#3366FF", alpha=0.3)
-		} else {
-			if (with_point) {
-				p <- p + geom_point()
-			}
-			p <- p + geom_boxplot(fill="grey80", colour="#3366FF", alpha=0.3)
+		if (with_point) {
+			p <- p + geom_point()
 		}
+		p <- p + geom_violin(fill="grey80", colour="#3366FF", alpha=0.3) 
+	} else if (type == "Dotplot") {
+		if (with_point) {
+			p <- p + geom_point()
+		}
+		p <- p + geom_boxplot(fill="grey80", colour="#3366FF", alpha=0.3)
+	} else {
+		if (with_point) {
+			p <- p + geom_point()
+		}
+		p <- p + geom_boxplot(fill="grey80", colour="#3366FF", alpha=0.3)
+	}
 	return(p)
 }
