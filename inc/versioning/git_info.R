a303abce1b125c3ce06d4f02fabb5b4efb6d3822
Stephan Grein <stephan.grein@uni-bonn.de>
2025-04-15 09:46:12 +0200

    Cleanup module versioning.

--- inc/versioning/git_info.R
+++ inc/versioning/git_info.R
@@ -4,39 +4,38 @@
 #' This function is used to create the git branch information for the UI
 ################################################################################
 get_git_information_from_repository <- function() {
-      repo <- git2r::repository(".")
-      head_ref <- git2r::repository_head(repo)
-      current_branch <- NULL
-      tag_name <- NULL
+   repo <- git2r::repository(".")
+   head_ref <- git2r::repository_head(repo)
+   current_branch <- NULL
+   tag_name <- NULL
 
-      if (git2r::is_branch(head_ref)) {
-         current_branch <- head_ref$name
-      }
+   if (git2r::is_branch(head_ref)) {
+      current_branch <- head_ref$name
+   }
 
-      if (inherits(head_ref, "git_commit")) {
-         commit_id <- head_ref$sha
-         all_tags <- git2r::tags(repo)
-         for (tag in all_tags) {
-            if (identical(tag$target, head_ref)) {
-               tag_name <- tag$name
-               break
-            }
+   if (inherits(head_ref, "git_commit")) {
+      commit_id <- head_ref$sha
+      all_tags <- git2r::tags(repo)
+      for (tag in all_tags) {
+         if (identical(tag$target, head_ref)) {
+            tag_name <- tag$name
+            break
          }
       }
+   }
 
-     current_tag <- tag_name
-     current_commit_id <- substring(git2r::commits(repo)[[1]]$sha, 1, 16)
-	  version_info <- NULL
+   current_tag <- tag_name
+   current_commit_id <- substring(git2r::commits(repo)[[1]]$sha, 1, 16)
+   version_info <- NULL
 
-      if (is.null(current_branch)) {
-         if (is.null(current_tag)) {
-            version_info <- paste(current_commit_id)
-
-         } else {
-            version_info <- paste(current_tag)
-         }
-      }  else {
-         version_info <- paste(current_branch, "@ commit: ", current_commit_id)
+   if (is.null(current_branch)) {
+      if (is.null(current_tag)) {
+         version_info <- paste(current_commit_id)
+      } else {
+         version_info <- paste(current_tag)
       }
-	return(version_info)
+   } else {
+      version_info <- paste(current_branch, "@ commit: ", current_commit_id)
+   }
+   return(version_info)
 }
